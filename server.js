01bd36f0bd3b20c440f4ae3f410008c4f678f8a3
Ronen Ahuvia <rahuvia@gmail.com>
2024-10-18 15:57:25 +0300

    Add basic frontend with HTML and CSS

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Serve the index.html file for the root route
 app.get('/', (req, res) => {
-  res.send('Hello World! Welcome to the English Learning App!');
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(port, () => {
