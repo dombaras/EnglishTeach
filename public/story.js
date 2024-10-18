082634046ae19f53891139f63e280793f18ed5fd
Ronen Ahuvia <rahuvia@gmail.com>
2024-10-18 17:32:31 +0300

    log to console when read a story button is clicked

--- public/story.js
+++ public/story.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const startReadingBtn = document.getElementById('start-reading');
     startReadingBtn.addEventListener('click', () => {
-        window.location.href = '/read-story';
+        console.log('Read a Story button clicked');
+        window.location.href = '/story';
     });
 });
